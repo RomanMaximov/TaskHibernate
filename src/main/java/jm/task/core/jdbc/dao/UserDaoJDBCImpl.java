884b9a041b1c55612378cdebc3f7f66c4e2bf6f8
RomanMaximov <gegenwart.xxx@gmail.com>
2021-10-18 16:13:36 +0300

    CoreTask

--- src/main/java/jm/task/core/jdbc/dao/UserDaoJDBCImpl.java
+++ src/main/java/jm/task/core/jdbc/dao/UserDaoJDBCImpl.java
@@ -7,49 +7,93 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class UserDaoJDBCImpl implements UserDao {
+    private Connection connection = null;
+
     public UserDaoJDBCImpl() {}
 
     public void createUsersTable() {
-        try (Statement statement = new Util().getConnection().createStatement()) {
+        try {
+            connection = Util.getConnection();
+            Statement statement = connection.createStatement();
             statement.executeUpdate("CREATE TABLE IF NOT EXISTS users "
                     + "(id INT PRIMARY KEY AUTO_INCREMENT, name VARCHAR(255), last_name VARCHAR(255), age INT)");
         } catch (SQLException e) {
             e.printStackTrace();
+        } finally {
+            if (connection != null) {
+                try {
+                    connection.close();
+                } catch (SQLException e) {
+                    e.printStackTrace();
+                }
+            }
         }
     }
 
     public void dropUsersTable() {
-        try (Statement statement = new Util().getConnection().createStatement()) {
+        try {
+            connection = Util.getConnection();
+            Statement statement = connection.createStatement();
             statement.executeUpdate("DROP TABLE IF EXISTS users");
         } catch (SQLException e) {
             e.printStackTrace();
+        } finally {
+            if (connection != null) {
+                try {
+                    connection.close();
+                } catch (SQLException e) {
+                    e.printStackTrace();
+                }
+            }
         }
     }
 
     public void saveUser(String name, String lastName, byte age) {
-        try (PreparedStatement state = new Util().getConnection().prepareStatement("INSERT INTO users (name, last_name, age) VALUES (?, ?, ?)")) {
+        try {
+            connection = Util.getConnection();
+            PreparedStatement state = connection.prepareStatement("INSERT INTO users (name, last_name, age) VALUES (?, ?, ?)");
             state.setString(1, name);
             state.setString(2, lastName);
             state.setByte(3, age);
             state.executeUpdate();
         } catch (SQLException e) {
             e.printStackTrace();
+        } finally {
+            if (connection != null) {
+                try {
+                    connection.close();
+                } catch (SQLException e) {
+                    e.printStackTrace();
+                }
+            }
         }
     }
 
     public void removeUserById(long id) {
-        try (PreparedStatement state = new Util().getConnection().prepareStatement("DELETE FROM users WHERE id = ?")) {
+        try {
+            connection = Util.getConnection();
+            PreparedStatement state = connection.prepareStatement("DELETE FROM users WHERE id = ?");
             state.setLong(1, id);
             state.executeUpdate();
         } catch (SQLException e) {
             e.printStackTrace();
+        } finally {
+            if (connection != null) {
+                try {
+                    connection.close();
+                } catch (SQLException e) {
+                    e.printStackTrace();
+                }
+            }
         }
     }
 
     public List<User> getAllUsers() {
         List<User> users = new ArrayList<>();
 
-        try (ResultSet rs = new Util().getConnection().createStatement().executeQuery("SELECT * FROM users")) {
+        try {
+            connection = Util.getConnection();
+            ResultSet rs = connection.createStatement().executeQuery("SELECT * FROM users");
             while (rs.next()) {
                 User user = new User(rs.getString("name"), rs.getString("last_name"),
                                     rs.getByte("age"));
@@ -58,15 +102,33 @@ public class UserDaoJDBCImpl implements UserDao {
             }
         } catch (SQLException e) {
             e.printStackTrace();
+        } finally {
+            if (connection != null) {
+                try {
+                    connection.close();
+                } catch (SQLException e) {
+                    e.printStackTrace();
+                }
+            }
         }
         return users;
     }
 
     public void cleanUsersTable() {
-        try (Statement statement = new Util().getConnection().createStatement()) {
+        try {
+            connection = Util.getConnection();
+            Statement statement = connection.createStatement();
             statement.executeUpdate("TRUNCATE TABLE users");
         } catch (SQLException e) {
             e.printStackTrace();
+        } finally {
+            if (connection != null) {
+                try {
+                    connection.close();
+                } catch (SQLException e) {
+                    e.printStackTrace();
+                }
+            }
         }
     }
 }
--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -1,8 +1,6 @@
 package jm.task.core.jdbc.util;
 
-import com.mysql.fabric.jdbc.FabricMySQLDriver;
 import java.sql.Connection;
-import java.sql.Driver;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
@@ -10,16 +8,14 @@ public class Util {
     private static final String url = "jdbc:mysql://localhost:3306/users?autoReconnect=true&useSSL=false";
     private static final String userName = "root";
     private static final String password = "TT555bur";
-    private Connection connection;
 
-    public Connection getConnection() {
+    public static Connection getConnection() {
         try {
-            Driver driver = new FabricMySQLDriver();
-            DriverManager.deregisterDriver(driver);
-            connection = DriverManager.getConnection(url, userName, password);
+            return DriverManager.getConnection(url, userName, password);
         } catch (SQLException e) {
+            System.out.println("Error: no connection to DB.");
             e.printStackTrace();
         }
-        return connection;
+        return null;
     }
 }
